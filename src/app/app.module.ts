08b873ddd8f475156d0f2ee77f9479b7ad788e8d
p.hill <parehill1@gmail.com>
2020-06-03 18:23:04 -0700

    Everything up and stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from "./messages/messages.component";
+import { layout } from "angular-ui-layout";
 
 @NgModule({
   declarations: [
@@ -21,4 +22,4 @@ import { MessagesComponent } from "./messages/messages.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
